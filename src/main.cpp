27edd7ef046d1044a7e79fe09e38f156639d84b5
Pablo Suarez <pas.suarez@gmail.com>
2021-02-01 12:46:27 -0300

    Windows 7 accessible

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ void setup(){
   //your other setup stuff...
   WiFi.softAP("esp-captive");
   Serial.print("Local IP Address:"); 
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.softAPIP());
   dnsServer.start(53, "*", WiFi.softAPIP());
    
   server.onNotFound(handleNotFound);
@@ -56,10 +56,8 @@ void loop(){
 
 void handleNotFound(AsyncWebServerRequest *request){
   
-  Serial.println ("Function: handleNotFound reached!!!!!!!");
-
   String path = request->url();
-  Serial.print("handleNotFound: ");
+  Serial.print("Requested File: ");
   Serial.println(path);
 
   if(loadFromSdCard(request)){return;}
@@ -85,50 +83,51 @@ void handleNotFound(AsyncWebServerRequest *request){
 
 
 bool loadFromSdCard(AsyncWebServerRequest *request) {
-      String path = request->url();
-      String dataType = "text/plain";
-      struct fileBlk {
-        File dataFile;
-      };
-      fileBlk *fileObj = new fileBlk;
+  String path = request->url();
+  String dataType = "text/plain";
+  struct fileBlk {
+    File dataFile;
+  };
+  fileBlk *fileObj = new fileBlk;
       
+  if(path.endsWith("/generate_204")) path = "/index.html"; 
+  if(path.endsWith("/ncsi.txt")) path = "/index.html"; 
+  if(path.endsWith("/connecttest.txt")) path = "/index.html";
+  if(path.endsWith("/redirect")) path = "/index.html";
+  if(path.endsWith("/")) path += "index.html";
+  
+  if(path.endsWith(".src")) path = path.substring(0, path.lastIndexOf("."));
+  else if(path.endsWith(".html")) dataType = "text/html";
+  else if(path.endsWith(".css")) dataType = "text/css";
+  else if(path.endsWith(".js")) dataType = "application/javascript";
+  else if(path.endsWith(".png")) dataType = "image/png";
+  else if(path.endsWith(".gif")) dataType = "image/gif";
+  else if(path.endsWith(".jpg")) dataType = "image/jpeg";
+  else if(path.endsWith(".ico")) dataType = "image/x-icon";
+  else if(path.endsWith(".xml")) dataType = "text/xml";
+  else if(path.endsWith(".pdf")) dataType = "application/pdf";
+  else if(path.endsWith(".zip")) dataType = "application/zip";
 
-      if(path.endsWith("/generate_204")) {path = "/index.html"; Serial.println("¡*************!");}
-      if(path.endsWith("/connecttest.txt")) {path = "/index.html"; Serial.println("¡*************!");}
-      if(path.endsWith("/redirect")) {path = "/index.html"; Serial.println("¡*************!");}
-      if(path.endsWith("/")) path += "index.htm";
-      if(path.endsWith(".src")) path = path.substring(0, path.lastIndexOf("."));
-      else if(path.endsWith(".html")) dataType = "text/html";
-      else if(path.endsWith(".css")) dataType = "text/css";
-      else if(path.endsWith(".js")) dataType = "application/javascript";
-      else if(path.endsWith(".png")) dataType = "image/png";
-      else if(path.endsWith(".gif")) dataType = "image/gif";
-      else if(path.endsWith(".jpg")) dataType = "image/jpeg";
-      else if(path.endsWith(".ico")) dataType = "image/x-icon";
-      else if(path.endsWith(".xml")) dataType = "text/xml";
-      else if(path.endsWith(".pdf")) dataType = "application/pdf";
-      else if(path.endsWith(".zip")) dataType = "application/zip";
-     
-      fileObj->dataFile  = SD.open(path.c_str());
-      if(fileObj->dataFile.isDirectory()){
-        path += "/index.htm";
-        dataType = "text/html";
-        fileObj->dataFile = SD.open(path.c_str());
-      }
+  fileObj->dataFile  = SD.open(path.c_str());
+  if(fileObj->dataFile.isDirectory()){
+    path += "/index.htm";
+    dataType = "text/html";
+    fileObj->dataFile = SD.open(path.c_str());
+  }
     
-      if (!fileObj->dataFile){
-        delete fileObj;
-        return false;
-      }
+  if (!fileObj->dataFile){
+    delete fileObj;
+    return false;
+  }
     
-      if (request->hasParam("download")) dataType = "application/octet-stream";
+  if (request->hasParam("download")) dataType = "application/octet-stream";
 
-            // Here is the context problem.  If there are multiple downloads active, 
-            // we don't have the File handles. So we only allow one active download request
-            // at a time and keep the file handle in static.  I'm open to a solution.
+  // Here is the context problem.  If there are multiple downloads active, 
+  // we don't have the File handles. So we only allow one active download request
+  // at a time and keep the file handle in static.  I'm open to a solution.
     
-      request->_tempObject = (void*)fileObj;
-      request->send(dataType, fileObj->dataFile.size(), [request](uint8_t *buffer, size_t maxlen, size_t index) -> size_t {
+  request->_tempObject = (void*)fileObj;
+  request->send(dataType, fileObj->dataFile.size(), [request](uint8_t *buffer, size_t maxlen, size_t index) -> size_t {
                                                   fileBlk *fileObj = (fileBlk*)request->_tempObject;
                                                   size_t thisSize = fileObj->dataFile.read(buffer, maxlen);
                                                   if((index + thisSize) >= fileObj->dataFile.size()){
@@ -138,7 +137,7 @@ bool loadFromSdCard(AsyncWebServerRequest *request) {
                                                   }
                                                   return thisSize;
                                                 });
-      return true;
+  return true;
 }
 
 
@@ -146,34 +145,27 @@ bool loadFromSdCard(AsyncWebServerRequest *request) {
 
 void printDirectory(File dir, int numTabs)
 {
-
-    while (true)
-    {
-        File entry = dir.openNextFile();
-        if (!entry)
-        {
-            // no more files
-            break;
-        }
-        if (numTabs > 0)
-        {
-            for (uint8_t i = 0; i <= numTabs; i++)
-            {
-                Serial.print('\t');
-            }
-        }
-        Serial.print(entry.name());
-        if (entry.isDirectory())
-        {
-            Serial.println("/");
-            printDirectory(entry, numTabs + 1);
-        }
-        else
-        {
-            // files have sizes, directories do not
-            Serial.print("\t");
-            Serial.println(entry.size(), DEC);
-        }
-        entry.close();
+  while (true){
+    File entry = dir.openNextFile();
+    if (!entry){
+     // no more files
+    break;
     }
+    
+    if (numTabs > 0){
+      for (uint8_t i = 0; i <= numTabs; i++){Serial.print('\t');}
+    }
+    
+    Serial.print(entry.name());
+    if (entry.isDirectory()){
+      Serial.println("/");
+      printDirectory(entry, numTabs + 1);
+    }
+    else{
+    // files have sizes, directories do not
+      Serial.print("\t");
+      Serial.println(entry.size(), DEC);
+    }
+    entry.close();
+  }
 }
